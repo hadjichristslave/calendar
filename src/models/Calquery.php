a4086de96df104db5f2bae31de42860d2633c567
Slave <p.hadjichristodoulou@hotmail.com>
2014-02-21 20:46:07 +0100

    asdf

---
+++ src/models/Calquery.php
@@ -0,0 +1,26 @@
+<?php 
+
+
+Class Calquery{
+
+
+	public $user_id;
+	public $count;
+
+	public function __construct($user_id , $count){
+		$this->user_id = $user_id;
+		$this->count   = $count;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+}
